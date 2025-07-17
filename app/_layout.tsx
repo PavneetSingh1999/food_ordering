3442b85dffa66e6a5a14c106f61658a7a15c4431
Gurupreet Singh Sachdev <gurupreetsinghsachdev@Gurupreets-MacBook-Air.local>
2025-07-17 19:20:48 +0530

    setup of food_app

---
+++ app/_layout.tsx
@@ -0,0 +1,37 @@
+import {SplashScreen, Stack} from "expo-router";
+import './globals.css'
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://b30b34c35b9cb910b080a19c217e423f@o4509684058619904.ingest.de.sentry.io/4509684097417296',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded,error]);
+  return <Stack screenOptions={{ headerShown: false }} />;
+});
